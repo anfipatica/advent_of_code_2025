84ae784fa99117d57f467d5f3e7cc87a60f4da06
anfi <anfi@anfi>
2025-12-06 00:19:55 +0100

    FUNCIONA Siento que es un desastre PERO FUNCIONAAAAAAA

--- 05/2/main.py
+++ 05/2/main.py
@@ -1,4 +1,7 @@
 
+START:	int = 0
+END:	int = 1
+
 def	add_ids_to_list(valid_ids: list[tuple[int, int]], start: int, end: int) -> list[tuple[int, int]]:
 
 	for index in range(0, len(valid_ids) + 1):
@@ -12,44 +15,71 @@ def	add_ids_to_list(valid_ids: list[tuple[int, int]], start: int, end: int) -> l
 			break
 	return valid_ids
 
-def	separate_file(file) -> list[tuple[int, int]]:
+def	merge_overlaping_ranges(valid_ids: list[list], start: int, end: int) -> list[list]:
+	overlap: bool = False
+	if not valid_ids:
+		valid_ids.append([start, end])
+
+	for id_range in valid_ids:
+		# SOLAPAMIENTO TOTAL
+		if start <= id_range[START] and end >= id_range[END]: # el nuevo rango absorbe el anterior
+			id_range[START] = start
+			id_range[END] = end
+			overlap = True
+		elif id_range[START] <= start and id_range[END] >= end: # el nuevo rango es absorbido
+			start = id_range[START]
+			end = id_range[END]
+			overlap = True
+		
+		# SOLAPAMIENTO PARCIAL
+		elif start >= id_range[START] and start <= id_range[END]: # el inicio del nuevo rango está dentro del otro
+			if end > id_range[END]: # partial overlap
+				start = id_range[START]
+				id_range[END] = end
+			else: # total overlap
+				start = id_range[START]
+				end = id_range[END]
+			overlap = True
+
+		elif end >= id_range[START] and end <= id_range[END]:
+			if start < id_range[END]:
+				end = id_range[END]
+				id_range[START] = start # partial overlap
+			else: # total overlap
+				start = id_range[START]
+				end = id_range[END]
+			overlap = True
+
+	# SIN SOLAPAMIENTO, SE CREA NUEVO RANGO
+	if overlap == False:
+		valid_ids.append([start, end])
+	return valid_ids
+
+
+def	separate_file(file) -> list[list]:
 
 	numbers_splitted: list[str]
-	valid_ids: list[tuple[int, int]] = []
+	ordered_ids: list[list] = []
+	valid_ids: list[list] = []
 	check_ids: list[int] = []
 
 	for line in file:
 		if line == "\n":
 			break
 		numbers_splitted = line.strip().split("-")
-		valid_ids = add_ids_to_list(valid_ids, int(numbers_splitted[0]), int(numbers_splitted[1]))
+		ordered_ids = add_ids_to_list(ordered_ids, int(numbers_splitted[0]), int(numbers_splitted[1]))
+	
+	for id_range in ordered_ids:
+		valid_ids = merge_overlaping_ranges(valid_ids, id_range[START], id_range[END])
 
+	
 	return valid_ids
 
-def	count_ids(id_list: list[tuple[int, int]], index: int) -> bool:
-	if not hasattr(count_ids, "biggest_id"):
-		count_ids.biggest_id = 0
-	id_n: int
-	print("biggest_number %d" %(count_ids.biggest_id))
-	if index != 0 and id_list[index][1] < count_ids.biggest_id: #total overlap, no new number
-		print("0.    0")
-		return 0
-	elif index != 0 and id_list[index][0] < id_list[index - 1][1]: # overlap parcial
-		id_n = id_list[index][1] - count_ids.biggest_id
-		print("1.    %d" %(id_n))
-	elif index != 0 and id_list[index][0] < count_ids.biggest_id:
-		id_n = id_list[index][1] - count_ids.biggest_id
-		print("3.    %d" %(id_n))
-	else:
-		id_n = id_list[index][1] - id_list[index][0] + 1
-		print("2.    %d" %(id_n))
-	if id_list[index][0] == count_ids.biggest_id or id_list[index][1] == count_ids.biggest_id:
-		id_n -= 1
-	count_ids.biggest_id = id_list[index][1]
-	return id_n
+def	count_ids(id_list: list[list], index: int) -> bool:
+	pass
 
 def	main():
-	valid_ids: list[tuple[int, int]] = []
+	valid_ids: list[list] = []
 	total_valid_ids: int = 0
 
 	with open("./input.txt", "r") as file:
@@ -57,16 +87,17 @@ def	main():
 		print("resulting list:")
 		for id in valid_ids:
 			print(id)
-		print("----------------")
-		for index in range(0, len(valid_ids)):
-			print(f"now checking: {valid_ids[index]}")
-			total_valid_ids += count_ids(valid_ids, index)
-			print("")
+		for id_range in valid_ids:
+			total_valid_ids += id_range[END] - id_range[START] + 1
+
 	print("RESULT: %d" %(total_valid_ids))
 
 
 ### 323763676291254 TOO LOW
 ### 360341832208384 
+### 360341832208314
+
+### 360341832208407
 ### 360608726043689 TOO HIGH
 ### 360608726043727 TOO HIGH
 
---
+++ test.py
@@ -0,0 +1,6 @@
+def main():
+	n1: float = 1.7976931348623157e+80
+	print(n1)
+
+if __name__ == "__main__":
+	main()
